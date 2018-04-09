4a35a927e93a83282578d574373a6430a5839194
easyMan-zzy <272210465@qq.com>
2018-04-09 10:18:35 +0800

    Initial commit

---
+++ js/mainfirst.js
@@ -0,0 +1,20 @@
+var subSys_hangzhou = "http://47.97.173.174:8081/front/kphlsecond.html";
+var subSys_ningbo = "http://47.97.173.174:8080/front/kphlsecond.html";
+var subSys_quzhou = "";
+var subSys_zhousan = "";
+
+$('#firstToHZ').click(function() {
+    window.location.href = subSys_hangzhou;
+});
+
+$('#firstToNB').click(function() {
+	window.location.href = subSys_ningbo;
+});
+
+$('#firstToQZ').click(function() {
+    window.location.href = subSys_quzhou;
+});
+
+$('#firstToZS').click(function() {
+    window.location.href = subSys_zhousan;
+});
