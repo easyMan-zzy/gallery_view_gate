0184923cf573b57d78452649904a97f5fccef9a6
easyMan-zzy <272210465@qq.com>
2018-04-09 10:22:09 +0800

    测试提交

--- js/mainfirst.js
+++ js/mainfirst.js
@@ -1,3 +1,4 @@
+//这里是要添加的城市链接
 var subSys_hangzhou = "http://47.97.173.174:8081/front/kphlsecond.html";
 var subSys_ningbo = "http://47.97.173.174:8080/front/kphlsecond.html";
 var subSys_quzhou = "";
