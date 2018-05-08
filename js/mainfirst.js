f377088f0aaf16184d03fbf09dd5305c3fb738fe
jingcherry <1570108978@qq.com>
2018-05-08 17:31:58 +0800

    修改mainfirst.js，zjProvince.html增加一张图片

--- js/mainfirst.js
+++ js/mainfirst.js
@@ -3,19 +3,19 @@ var subSys_hangzhou = "http://47.97.173.174:8081/front/kphlsecond.html";
 var subSys_ningbo = "http://47.97.173.174:8080/front/kphlsecond.html";
 var subSys_quzhou = "http://kphl.qzkx.gov.cn:8081/front/kphlsecond.html";
 var subSys_zhousan = "";
-
-$('#firstToHZ').click(function() {
+//杭州
+$('.hangzhou').click(function() {
     window.location.href = subSys_hangzhou;
 });
-
-$('#firstToNB').click(function() {
-	window.location.href = subSys_ningbo;
-});
-
-$('#firstToQZ').click(function() {
+//衢州
+$('.quzhou').click(function() {
     window.location.href = subSys_quzhou;
 });
-
-$('#firstToZS').click(function() {
+//宁波
+$('.ningbo').click(function() {
+	window.location.href = subSys_ningbo;
+});
+//舟山
+$('.zhoushan').click(function() {
     window.location.href = subSys_zhousan;
 });
